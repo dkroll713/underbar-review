036d50e672ef88e3ea978686fcdb68971b8d4a3e
Joe Shultz josephcshultz@gmail.com <josephcshultz@gmail.com>
2022-06-01 10:56:12 -0600

    passing each

--- src/underbar.js
+++ src/underbar.js
@@ -42,9 +42,9 @@
       return array.pop();
     } else if( n === 0 ) {
       return [];
+    } else if(n > array.length) {
+      return array;
     }
-
-
 
     var result = [];
 
@@ -62,6 +62,17 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+
+    if(Array.isArray(collection)) {
+      for ( var x = 0; x < collection.length; x++ ) {
+        iterator(collection[x], x, collection);
+      }
+    } else {
+      for ( var key in collection ) {
+        iterator(collection[key], key, collection);
+      }
+    }
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
